eb8e5fef2f6ef690f660c4947b3a341029abe314
30005209 <30005209@students.southwales.ac.uk>
2022-08-15 16:56:58 +0100

    added help url linekd to issue

--- Assets/Stat.cs
+++ Assets/Stat.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [System.Serializable]
+[HelpURL("https://github.com/IJCoding/FromThineEyes/issues/6")]
 public class Stat : MonoBehaviour
 {
-    [SerializeField]    private int _min;
-    [SerializeField]    private int _cur;
-    [SerializeField]    private int _max;
+    [SerializeField]    private int _min = 0;
+    [SerializeField]    private int _cur = 0;
+    [SerializeField]    private int _max = 0;
 
-    public Stat(int defMin = 0, int defCur = 6, int defMax = 12)
+    public Stat(int defMin = 0, int defCur = 0, int defMax = 0)
     {
         RedefineBounds(defMin, defMax);
         AlterCur(defCur);
     }
-
 
     public void RedefineBounds(int newMin, int newMax)
     {
@@ -54,6 +55,5 @@ public class Stat : MonoBehaviour
     public int GetCur() { return _cur; }
     public int GetMax() { return _max; }
     public int GetMin() { return _min; }
-
 
 }
