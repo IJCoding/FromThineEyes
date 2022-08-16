3c5bef70cc0d914fca15e8ee687a2b8dec9f989d
30005209 <30005209@students.southwales.ac.uk>
2022-08-16 16:31:43 +0100

    Trait added with custom inspector

--- Assets/Effect.cs
+++ Assets/Effect.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "newEffect", menuName = "Effects")]
 public class Effect : ScriptableObject
 {
-    public string GetName() { return this.name; }
+    [SerializeField]
+    private string _name;
+
+    private void OnEnable()
+    {
+        _name = this.name;
+        if (_name.Length == 0) { _name = "Undefined"; }
+    }
+
+    public string GetName() { return _name; }
 }
